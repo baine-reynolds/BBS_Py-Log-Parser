7dc9ef33485ab5986f0f5ce468bcbcaf943982d7
baine-reynolds <baine.server@gmail.com>
2020-06-04 09:46:56 -0500

    added options to dump to json for testing and validation

--- pylogparser.py
+++ pylogparser.py
@@ -4,20 +4,21 @@ from resources.parser import Parser
 import json
 
 
-def export_json(all_access_logs):
+def export_json(hourly_breakdown, system_stats):
     with open('access_logs_parsed.json', 'w') as out_file:
-        json.dump(all_access_logs, out_file)
+        json.dump(hourly_breakdown, out_file)
+        json.dump(system_stats, out_file)
     
 
 def main():
     options, args = Init.parse_input()
     all_access_logs = Init.validate_path(options.filepath)
-    parsed_logs = Parser.start(all_access_logs)
+    hourly_breakdown, system_stats = Parser.start(all_access_logs)
     if options.json == True:
-        export_json(all_access_logs)
+        export_json(hourly_breakdown, system_stats)
         exit("Created 'access_logs_parsed.json' in working directory")
     else:
-        Graph.graph_parsed(parsed_logs)
+        Graph.graph_parsed(hourly_breakdown, system_stats)
         exit("Created pdfs in working directory")
 
 
