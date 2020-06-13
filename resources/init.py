ea9edbd6e1d54e17270ee4f3d9ec4fe0bc9947b6
baine-reynolds <baine.server@gmail.com>
2020-06-13 16:11:28 -0500

    converted path validation to a dict where keys are node names for graphing purposes from a standard list of all access logs

--- resources/init.py
+++ resources/init.py
@@ -13,20 +13,48 @@ class Init:
         options, args = parser.parse_args()
         return options, args
 
-    def validate_path(path):
+    def validate_path(provided_path):
         '''
         Accepts the file path given to ensure that atleast 1 file matches the access log naming convention.
         Returns a list of full paths (os.path) to each file located/matched to "atlassian-bitbucket-access*".
         '''
+        all_nodes = {}
         if path is not None:
-            all_files = []
-            for root, dirs, files in os.walk(path):
+            for root, dirs, files in os.walk(provided_path):
                 for name in files:
                     if fnmatch.fnmatch(name, "atlassian-bitbucket-access*"):
-                        all_files.append(os.path.join(root, name))
-            if len(all_files) > 0:
+                        #all_files.append(os.path.join(root, name))
+                        file_path = os.path.join(root, name)
+                        node = identify_node(file_path, provided_path)
+                        if node not in all_nodes.keys():
+                            all_nodes[node] = []
+                        all_nodes[node].append(file_path)
+            if len(all_nodes.keys()) > 0:
+                # if logs and nodes were identified, continue
                 return all_files
             else:
                 exit("Could not locate any access logs in given path. Please check the path and try again.")
         else:
             exit("No path specificed, please use the '-d' flag with a path to the access logs.")
+
+    def identify_node(file_path, provided_path):
+        # Reduces path to only the sub-directories after the given parent path
+        short_path = file_path.replace(provided_path, "")
+        list_short_path = short_path.split('/')
+        try:
+            if "access" in list_short_path[3]:
+                node = list_short_path[1]:
+        except IndexError:
+            try:
+                if "access" in list_short_path[2]:
+                    # Assume single node is given, supply "Node 1" as the node name
+                    node = "Node 1"
+            except IndexError:
+                try:
+                    if "access" in list_short_path[1]:
+                        # Assume single node is given, supply "Node 1" as the node name
+                        node = "Node 1"
+                except IndexError:
+                    exit("Access logs not within expected folder structure, please check the path given and try again.")
+        return node
+
