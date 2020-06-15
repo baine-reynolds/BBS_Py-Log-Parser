cdad6d7430857082889f1cc10140b6642248ea7e
baine-reynolds <baine.server@gmail.com>
2020-06-14 20:22:11 -0500

    adding additional graphs [fetches, pushes] | fetches need to be looked at as it does not match upstream target repo

--- resources/graph.py
+++ resources/graph.py
@@ -4,13 +4,13 @@ class Graph:
     def graph_parsed(node, hourly_breakdown, system_stats, dark_mode):
         all_chrono_days = Graph.sort_days(hourly_breakdown.keys())
         Graph.clones(node, all_chrono_days, hourly_breakdown, dark_mode)
-        #Graph.fetches(sorted_hourly_breakdown)
-        #Graph.pushes(sorted_hourly_breakdown)
-        #Graph.ref_ads(sorted_hourly_breakdown)
-        #Graph.shallow_clones(sorted_hourly_breakdown)
-        #Graph.summary(sorted_hourly_breakdown)
-        #Graph.max_connections(sorted_hourly_breakdown)
-        #Graph.protocols(sorted_hourly_breakdown)
+        Graph.fetches(node, all_chrono_days, hourly_breakdown, dark_mode)
+        Graph.pushes(node, all_chrono_days, hourly_breakdown, dark_mode)
+        #Graph.ref_ads(node, all_chrono_days, hourly_breakdown, dark_mode)
+        #Graph.shallow_clones(node, all_chrono_days, hourly_breakdown, dark_mode)
+        #Graph.summary(node, all_chrono_days, hourly_breakdown, dark_mode)
+        #Graph.max_connections(node, all_chrono_days, hourly_breakdown, dark_mode)
+        #Graph.protocols(node, all_chrono_days, hourly_breakdown, dark_mode)
 
         #Graph.repos(system_stats['repo_stats'], dark_mode)
         Graph.operations(node, system_stats['operations'], dark_mode)
@@ -53,7 +53,7 @@ class Graph:
         if dark_mode:
             textprops = {'color': 'w'}
             plt.style.use('dark_background')
-        plt.plot(date_labels, clone_all_data, 'b--', label="Clone") 
+        plt.plot(date_labels, clone_all_data, 'b--', label="All Clones") 
         plt.plot(date_labels, clone_hit_data, 'g.-', label="Clone/Cache Hit")
         plt.plot(date_labels, clone_miss_data, 'r.-', label="Clone/Cache Miss")
         #plt.plot([], operation], labels=labels, textprops=textprops)
@@ -65,25 +65,91 @@ class Graph:
 
         plt.savefig(f'{node}-clones.png')
 
-    def fetches(hourly_breakdown):
+    def fetches(node, all_chrono_days, hourly_breakdown, dark_mode):
+        fetch_all_data = []
+        fetch_hit_data = []
+        fetch_miss_data = []
+        date_labels = []
+        hours = range(0, 24)
+        for day in all_chrono_days:
+            for hour in hours:
+                date_to_use = f"{str(day)[:4]}-{str(day)[4:6]}-{str(day)[6:8]} {hour:02d}"
+                date_labels.append(str(date_to_use))
+                try:
+                    fetch_all_data.append(hourly_breakdown[day][hour]['total_fetches'] + hourly_breakdown[day][hour]['total_fetch_misses'])
+                    fetch_hit_data.append(hourly_breakdown[day][hour]['total_fetches'])
+                    fetch_miss_data.append(hourly_breakdown[day][hour]['total_fetch_misses'])
+                except KeyError:
+                    # start of day may not be at 0 so this fills in the blanks to have a complete 24 hour day
+                    fetch_all_data.append(0)
+                    fetch_hit_data.append(0)
+                    fetch_miss_data.append(0)
+
+        plt.figure(figsize=(24,10))
+        plt.subplots_adjust(top=0.96, bottom=0.05, left=0.04, right=0.99, wspace=0.9)
+        # bottom=0, right=1, left=0, hspace=0, wspace=0)
+        plt.margins(0.01,0.05)
+        textprops={'color': 'black'}
+        if dark_mode:
+            textprops = {'color': 'w'}
+            plt.style.use('dark_background')
+        plt.plot(date_labels, fetch_all_data, 'b--', label="All Fetches") 
+        plt.plot(date_labels, fetch_hit_data, 'g.-', label="Fetch/Cache Hit")
+        plt.plot(date_labels, fetch_miss_data, 'r.-', label="Fetch/Cache Miss")
+        #plt.plot([], operation], labels=labels, textprops=textprops)
+        plt.title(f"Fetch Operations ({node})", fontdict={'fontweight': 'bold', 'fontsize': 20})
+        plt.xlabel('')
+        plt.legend()
+        plt.xticks(date_labels[::24])
+        plt.ylabel('Number of Fetches', fontdict={'fontweight': 'bold', 'fontsize': 14})
+
+        plt.savefig(f'{node}-fetches.png')
+
+    def pushes(node, all_chrono_days, hourly_breakdown, dark_mode):
+        push_all_data = []
+        date_labels = []
+        hours = range(0, 24)
+        for day in all_chrono_days:
+            for hour in hours:
+                date_to_use = f"{str(day)[:4]}-{str(day)[4:6]}-{str(day)[6:8]} {hour:02d}"
+                date_labels.append(str(date_to_use))
+                try:
+                    push_all_data.append(hourly_breakdown[day][hour]['total_pushes'])
+                except KeyError:
+                    # start of day may not be at 0 so this fills in the blanks to have a complete 24 hour day
+                    push_all_data.append(0)
+
+        plt.figure(figsize=(24,10))
+        plt.subplots_adjust(top=0.96, bottom=0.05, left=0.04, right=0.99, wspace=0.9)
+        # bottom=0, right=1, left=0, hspace=0, wspace=0)
+        plt.margins(0.01,0.05)
+        textprops={'color': 'black'}
+        if dark_mode:
+            textprops = {'color': 'w'}
+            plt.style.use('dark_background')
+        plt.plot(date_labels, push_all_data, 'b.-', label="All pushes") 
+        #plt.plot([], operation], labels=labels, textprops=textprops)
+        plt.title(f"push Operations ({node})", fontdict={'fontweight': 'bold', 'fontsize': 20})
+        plt.xlabel('')
+        plt.legend()
+        plt.xticks(date_labels[::24])
+        plt.ylabel('Number of pushes', fontdict={'fontweight': 'bold', 'fontsize': 14})
+
+        plt.savefig(f'{node}-pushes.png')
+
+    def ref_ads(node, all_chrono_days, hourly_breakdown, dark_mode):
         pass
 
-    def pushes(hourly_breakdown):
+    def shallow_clones(node, all_chrono_days, hourly_breakdown, dark_mode):
         pass
 
-    def ref_ads(hourly_breakdown):
+    def summary(node, all_chrono_days, hourly_breakdown, dark_mode):
         pass
 
-    def shallow_clones(hourly_breakdown):
+    def max_connections(node, all_chrono_days, hourly_breakdown, dark_mode):
         pass
 
-    def summary(hourly_breakdown):
-        pass
-
-    def max_connections(hourly_breakdown):
-        pass
-
-    def protocols(hourly_breakdown):
+    def protocols(node, all_chrono_days, hourly_breakdown, dark_mode):
         pass
 
     def repos(repo_stats, dark_mode):
