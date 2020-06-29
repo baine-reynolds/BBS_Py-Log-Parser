8c3dae82eb593b5212e9370e4a3c2b364529dba2
baine-reynolds <baine.server@gmail.com>
2020-06-28 23:56:06 -0500

    cleanup

--- resources/graph.py
+++ resources/graph.py
@@ -24,7 +24,7 @@ class Graph:
         #Graph.repos(node, system_stats['repo_stats'])
         Graph.operations(node, system_stats['operations'])
         plt.close("all")
-        
+
         return
 
     def sort_days(hourly_breakdown_keys):
@@ -381,37 +381,3 @@ class Graph:
         plt.savefig(f'{node}-operations.jpg', dpi=500)
         plt.clf()
         return("Operations")
-
-
-
-
-'''
-graph 1:
-    total clones (cache hit vs miss)
-graph 2:
-    total fetches (cache hit vs miss)
-graph 3:
-    total pushes
-graph 4:
-    total refs advs (cache hit vs miss)
-graph 5:
-    total shallow clones (hit vs miss)
-graph 6:
-    total clone, total fetch, total shallow clone, total push   (ref advs secondary y axis)
-graph 7:
-    max concurrent operations
-graph 8:
-    Git http(s) vs ssh operations
-
-
-graph 9:
-    top 10 repo clones
-graph 10:
-    top 10 repo fetches
-graph 11:
-    top 10 repo pushes
-graph 12:
-    top 10 repo refs
-graph 13:
-    distribution of operations (webUI, git ssh, git http(s) rest, file server)
-'''
